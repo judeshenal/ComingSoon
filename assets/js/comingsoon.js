4ae3a8e59efe0ad2352eadde94c21ebe8528e59b
judeshenal <2000daredevil@gmail.com>
2023-07-28 18:35:28 +0530

    Update date

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Aug 3, 2023 00:00:00").getTime();
+    const countDate = new Date("Aug 2, 2023 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
